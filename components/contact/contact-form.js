f5ae1c6c1a279a472e2e6a451e60985d4592a052
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-26 20:20:15 +0200

    sending data from the client to the API route

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,23 +1,63 @@
+import { useState } from "react";
 import styles from "./contact-form.module.css";
 
 const ContactForm = () => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  const sendMessageHandler = (e) => {
+    e.preventDefault();
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    }).then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <section className={styles.contact}>
       <h1>How can I help you?</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={sendMessageHandler}>
         <div className={styles.controls}>
           <div className={styles.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(e) => setEnteredEmail(e.target.value)}
+            />
           </div>
           <div className={styles.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(e) => setEnteredName(e.target.value)}
+            />
           </div>
         </div>
         <div className={styles.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows={5}></textarea>
+          <textarea
+            id="message"
+            rows={5}
+            required
+            value={enteredMessage}
+            onChange={(e) => setEnteredMessage(e.target.value)}
+          ></textarea>
         </div>
         <div className={styles.actions}>
           <button>Send Message</button>
